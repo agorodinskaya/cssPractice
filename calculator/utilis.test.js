11c4b0971b37b3e9e81332a239d81583ddf0c40c
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-08-19 23:49:41 +1000

    13 tests pass ✅

--- calculator/utilis.test.js
+++ calculator/utilis.test.js
@@ -1,11 +1,20 @@
 import { operations } from "./utilis";
 import { clear } from "./utilis";
-test("test works", () => {
-  expect(1).toBe(1);
+// test("test works", () => {
+//   expect(1).toBe(1);
+// });
+beforeAll(() => {
+  const calculator = document.querySelector(".calculator");
+  const buttons = document.querySelector(".buttons");
+  const display = document.querySelector(".display");
 });
 
 // testing operations function :
 // 2 unit tests for add :
+
+test("adds 2 + 2 to equals 4.00", () => {
+  expect(operations(2, "add", 2)).toBe("4.00");
+});
 
 test("adds 2 + 2 to NOT equals 5.00", () => {
   expect(operations(2, "add", 2)).not.toBe("5.00");
@@ -44,7 +53,7 @@ test("decimals 3.2 + 5.2 equals 8.40", () => {
 });
 
 //  1 unit test for divide by 0 - error = infinity- display error message:
-test("error message 6 / 0 equals Infinity", () => {
+test("when divided by 0, e.g.  6 / 0 equals Infinity", () => {
   expect(operations(6, "divide", 0)).toBe("Infinity");
 });
 
@@ -67,5 +76,3 @@ test("values are set to zero or empty string", () => {
   expect(clear().previousKeyType).toBe("");
   expect(clear().textContent).toBe(0);
 });
-
-// decimal testing for display:
